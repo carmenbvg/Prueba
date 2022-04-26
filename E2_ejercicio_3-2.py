bb4ab15705263cb5361441ecbe656a7955493f5b
Carmenbvg <carmenbvg@gmail.com>
2022-04-26 20:21:20 +0200

    Add files via upload

---
+++ E2_ejercicio_3-2.py
@@ -0,0 +1,6 @@
+# 2 EJERCICIO TEMA 2 TKINTER
+
+# Crea una ventana interfaz de usuario que sirva para generar una lista con productos para ir a la compra
+# Para ello, utiliza tkinter, creando la raiz, etiquetas y campo de texto
+# Almacena los nombres de cinco productos como elementos de una lista, a medida que se introduzcan.
+# Finalmente, muestra los elementos de la lista
